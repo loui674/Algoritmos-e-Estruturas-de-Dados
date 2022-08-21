a137c507e08c840ca87bf0b7f5707ef188a8ddde
Luisa <101749657+loui674@users.noreply.github.com>
2022-08-21 11:05:40 -0300

    Fila de Produtos
    Fila de Produtos armazenados em uma Pilha que, armazena os dados "Nome" e "Preço" em uma Class Produto

---
+++ Fila_Produto/Fila_Produto/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fila_Produto
+{
+    internal class Produto
+    {
+        public string NomeProduto { get; set; }
+        public double PrecoProduto { get; set; }
+    }
+}
---
+++ Fila_Produto/Fila_Produto/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fila_Produto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Stack<Produto>> MinhaFIla = new Queue<Stack<Produto>>();
+
+            Stack<Produto> MinhaPilha;
+
+            Produto xProduto;
+
+            MinhaPilha = new Stack<Produto>();
+
+            xProduto = new Produto();
+
+            xProduto.NomeProduto = "Pasta ZIP 20 Furos";
+            xProduto.PrecoProduto = 54.30;
+
+            MinhaPilha.Push(xProduto);
+
+            xProduto = new Produto();
+
+            xProduto.NomeProduto = "Caderno Yasuo League of Legends";
+            xProduto.PrecoProduto = 24.30;
+
+            MinhaPilha.Push(xProduto);
+
+            xProduto = new Produto();
+
+            xProduto.NomeProduto = "Pasta Rosa Panini";
+            xProduto.PrecoProduto = 12.80;
+
+            MinhaPilha.Push(xProduto);
+
+            MinhaFIla.Enqueue(MinhaPilha);
+
+
+            MinhaPilha = new Stack<Produto>();
+
+            xProduto = new Produto();
+
+            xProduto.NomeProduto = "Whiskey Chivas Regal";
+            xProduto.PrecoProduto = 190;
+
+            MinhaPilha.Push(xProduto);
+
+            xProduto = new Produto();
+
+            xProduto.NomeProduto = "Fanta Guaraná Delicinha";
+            xProduto.PrecoProduto = 4.00;
+
+            MinhaPilha.Push(xProduto);
+
+            MinhaFIla.Enqueue(MinhaPilha);
+
+            foreach(Stack<Produto> pilha in MinhaFIla)
+            {
+                Console.WriteLine("Dados da Pilha: ");
+
+                foreach(Produto prod in pilha)
+                {
+                    Console.WriteLine($"=> {prod.NomeProduto} - R$ {prod.PrecoProduto}");
+                }
+            }
+        }
+    }
+}
