3224e5f727306c3728b4def0d97beb5f5e4a9b76
Luisa <101749657+loui674@users.noreply.github.com>
2022-08-21 11:18:08 -0300

    Sorteia Elementos de um Vetor
    Programa em C# que por meio de funções faz um programa que sorteie elementos de um vetor de tamanho “T” fornecido pelo usuário e inverta a ordem de seus elementos.

---
+++ Sorteia_Elementos_Vetor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questao_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Por meio de funções faça um programa que sorteie elementos de um vetor de tamanho “T” 
+            fornecido pelo usuário e inverta a ordem de seus elementos.*/
+            int t;
+            Random x= new Random();
+
+            Console.Write("Insira o tamanho do vetor: ");
+            t=int.Parse(Console.ReadLine());
+            
+            int[] vet= new int [t];
+
+            PreencheVetor(vet,x);
+
+            Console.Write("Vetor na ordem original: ");
+            MostraVetor(vet);
+
+            Console.WriteLine();
+            InverteVetoreExibe(vet);
+
+            Console.ReadKey();
+        }
+        static void InverteVetoreExibe(int [] vet)
+        {
+            Array.Reverse(vet);
+            Console.Write("Ordem inversa: ");
+            for(int i = 0; i < vet.Length; i++)
+            {
+                Console.Write($"{vet[i],6}");
+            }
+        }
+        static void MostraVetor(int[] vet)
+        {
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write($"{vet[i],7}");
+            }
+        }
+        static void PreencheVetor(int[] vet, Random x)
+        {
+            x = new Random();
+            for (int i = 0; i < vet.Length; i++)
+            {
+                vet[i] = x.Next(0, 50);
+            }
+        }
+    }
+}
