1742fac8ba70be00dbd7df045ad31518065c591b
Luisa <101749657+loui674@users.noreply.github.com>
2022-08-21 11:12:47 -0300

    Métodos Construtores e Destrutores
    Aplicando na prática Construtores e Destrutores em uma class Pessoa

---
+++ Métodos Construtores/Métodos Construtores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Métodos_Construtores
+{
+    class Pessoa
+    {
+        int idade;
+        //Método Construtor com e sem uso de parâmetros
+        public Pessoa()
+        {
+            Console.WriteLine("Objeto Pessoa criado.");
+        }
+        public Pessoa(int idade) : this ()
+        {
+            this.idade = idade;
+        }
+        //Método Destrutor
+       ~Pessoa()
+        {
+            Console.WriteLine("Objeto Pessoa Destruído. ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
